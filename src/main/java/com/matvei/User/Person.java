f28b2b90e8e4d702138706a5a974f730827a556e
Муруев-МВ <matvei_muruev@mail.ru>
2024-07-29 18:07:49 +0800

    second

---
+++ src/main/java/com/matvei/User/Person.java
@@ -0,0 +1,14 @@
+package com.matvei.User;
+
+import lombok.Data;
+
+@Data
+public class Person {
+
+    private String name;
+    private String surname;
+    private String family;
+
+
+
+}
--- src/main/java/com/matvei/User/UserApplication.java
+++ src/main/java/com/matvei/User/UserApplication.java
@@ -3,11 +3,17 @@ package com.matvei.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class UserApplication {
 
 	public static void main(String[] args) {
+
+		Person person = new Person();
+
+
 		SpringApplication.run(UserApplication.class, args);
 	}
+
 
 }
